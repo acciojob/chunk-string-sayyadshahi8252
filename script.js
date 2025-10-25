28995060c0c0847883753a5e783c8a84b17e12c9
sayyadshahi8252 <167566299+sayyadshahi8252@users.noreply.github.com>
2025-10-25 13:11:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function stringChop(str, size) {
-  // your code here
+  if (!str) return []; 
+
+  let arr = [];
+  let left = 0;
+  size = Number(size); // convert chunk size to number
+
+  while (left < str.length) {
+    arr.push(str.slice(left, left + size));
+    left += size;
+  }
+
+  return arr;
 }
 
 // Do not change the code below
